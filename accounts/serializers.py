3dd4cf44fb45908a2ae167ea3af191899bd0f0e0
Tanmay Thole <tanmaythole@gmail.com>
2022-02-19 16:25:12 +0530

    user serializer updated

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -4,10 +4,10 @@ from accounts.models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model=User
-        fields = ["email", "password"]
+        fields = ["email", "first_name", "last_name", "username", "mobile", "password"]
 
     def create(self, validated_data):
-        user = User.objects.create(email=validated_data['email'])
+        user = super().create(validated_data)
         user.set_password(validated_data['password'])
         user.save()
         return user
