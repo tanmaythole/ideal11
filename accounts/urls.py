c79f6bb50c554c474eefdf9adede4b1fa3b6b92f
Tanmay Thole <tanmaythole@gmail.com>
2022-02-19 16:19:01 +0530

    perform operations like forgot password, change password

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 from accounts.views import *
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('register/', UserRegisterAPI.as_view()),
@@ -7,4 +11,11 @@ urlpatterns = [
 
     path('verify-email/<str:token>/', VerifyEmail.as_view()),
     path('resendemail/', ResendEmailAPI.as_view()),
+    path('forgot-password/', ForgotPasswordAPI.as_view()),
+    path('reset-password/', ResetPasswordAPI.as_view()),
+    path('change-password/', ChangePasswordAPI.as_view()),
+
+
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -4,7 +4,8 @@ from accounts.serializers import UserSerializer
 from django.contrib.auth.models import auth
 from rest_framework_simplejwt.tokens import RefreshToken
 from .models import User
-from .helpers import send_mail_link
+from .helpers import send_forgotPassword_mail, send_mail_link
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 class UserRegisterAPI(APIView):
@@ -74,3 +75,73 @@ class ResendEmailAPI(APIView):
             return Response({"status":"ok", "message":"Email Sent Successfully"}, 200)
         return Response({"status":"Failed", "message":"Something Went Wrong"}, 401)
     
+
+
+class ForgotPasswordAPI(APIView):
+    def post(self, request):
+        data = request.data
+        is_mail_sent = send_forgotPassword_mail(User.objects.filter(email=data['email'])[0])
+        if (is_mail_sent):
+            return Response({
+                    "status":"ok",
+                    "message":"Email Sent Successfully"
+                }, 200)
+        
+        return Response({
+                "status":"Failed", 
+                "message":"Invalid Credentials"
+            }, 401)
+
+
+class ResetPasswordAPI(APIView):
+    def post(self, request):
+        token = request.data['token']
+        newPass = request.data['newPassword']
+        user = User.objects.filter(forgot_password_token=str(token))
+        if user.exists():
+            user = user[0]
+            user.set_password(newPass)
+            user.save()
+            return Response({
+                    "status":"ok",
+                    "message": "Password Updated Successfully!"
+                }, 200)
+        
+        return Response({
+                "status": "failed",
+                "message": "Something Went Wrong!"
+            }, 401)
+
+
+class ChangePasswordAPI(APIView):
+    def get_object(self):
+        user = self.request.user
+        return user
+        
+    permission_classes = (IsAuthenticated,)
+
+    def put(self, request):
+        user = self.get_object()
+        data = request.data
+
+        if data['newPassword']!=data['conNewPassword']:
+            return Response({"status":"failed", "newPassword":"Password didn't Match!"}, 400)
+        
+        if not user.check_password(data['old_password']):
+            return Response({"status":"failed", "old_password": ["Wrong password."]}, 400)
+        
+        try:
+            user.set_password(data['newPassword'])
+            user.save()
+
+            return Response({
+                "status": "ok",
+                "message": "Password Changed Successfully!"
+            }, 204)
+        except:
+            return Response({
+                'status': 'failed',
+                'message': "Something Went Wrong!"
+            }, 400)
+
+            
